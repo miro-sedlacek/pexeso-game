c7462c3cb6b6dd83be4e51e38b0daabe04f9ddec
miro-sedlacek <67795813+miro-sedlacek@users.noreply.github.com>
2023-02-20 01:30:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const cards = [
-	{ id: 1, img: 'image1.png' },
+	{ id: 1, img: 'Podcast Episode (1).jpg' },
 	{ id: 2, img: 'image2.png' },
 	{ id: 3, img: 'image3.png' },
 	{ id: 4, img: 'image4.png' },
